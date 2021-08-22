31f810ddaaf30e2504239d8f3d4cf7a64e6c7283
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2021-08-22 16:48:58 +0800

    first attempt at adding table rows there's an error tho, can't add multiple edittexts in one row for some reason

--- app/src/main/java/com/mobdeve/s17/lim/matthew/mobdeve_progplan/CreateProgActivity.java
+++ app/src/main/java/com/mobdeve/s17/lim/matthew/mobdeve_progplan/CreateProgActivity.java
@@ -3,6 +3,11 @@ package com.mobdeve.s17.lim.matthew.mobdeve_progplan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableRow;
 
 import com.mobdeve.s17.lim.matthew.mobdeve_progplan.databinding.ActivityCreateProgBinding;
 
@@ -24,6 +29,29 @@ public class CreateProgActivity extends AppCompatActivity {
 
 		binding.ivAddResource.setOnClickListener(v -> {
 			// TODO: add table row
+			TableRow newRow = new TableRow(this);
+			EditText name, value;
+			name = value = new EditText(this);
+
+			name.setBackground(getDrawable(R.drawable.border));
+			name.setLayoutParams(new TableRow.LayoutParams(
+					0,
+					TableRow.LayoutParams.MATCH_PARENT,
+					0.7f));
+			name.setTextColor(getColor(R.color.black));
+			newRow.addView(name);
+
+			value.setBackground(getDrawable(R.drawable.border));
+			value.setLayoutParams(new TableRow.LayoutParams(
+					0,
+					TableRow.LayoutParams.MATCH_PARENT,
+					0.3f));
+			value.setTextColor(getColor(R.color.black));
+			// The specified child already has a parent. You must call removeView() on the child's parent first.
+			// newRow.addView(value);
+
+			binding.tlResources.addView(newRow);
+			Log.d("TableLayout", "TableRow Count: " + binding.tlResources.getChildCount());
 		});
 
 		binding.ivAddOutcome.setOnClickListener(v -> {
