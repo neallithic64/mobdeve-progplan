9080bf8af51a4f75dc5b808740f18e6fca7d8a46
Andre Garcia <61376752+Episki2001@users.noreply.github.com>
2021-09-01 03:24:12 +0800

    Fixed Evaluate Programs views and intents

--- app/src/main/java/com/mobdeve/s17/lim/matthew/mobdeve_progplan/EvaluateProgActivity.java
+++ app/src/main/java/com/mobdeve/s17/lim/matthew/mobdeve_progplan/EvaluateProgActivity.java
@@ -2,6 +2,7 @@ package com.mobdeve.s17.lim.matthew.mobdeve_progplan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.mobdeve.s17.lim.matthew.mobdeve_progplan.databinding.ActivityEvaluateProgBinding;
@@ -14,5 +15,20 @@ public class EvaluateProgActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		binding = ActivityEvaluateProgBinding.inflate(getLayoutInflater());
 		setContentView(binding.getRoot());
+	}
+
+	private void initOnClick(){
+		binding.btnEvalCancel.setOnClickListener( v -> {
+			Intent gotoViewIndivProg = new Intent(EvaluateProgActivity.this, ViewIndivProgActivity.class);
+			startActivity(gotoViewIndivProg);
+			finish();
+		});
+
+		binding.btnEvalSave.setOnClickListener( v -> {
+//      TODO : Save changes to database
+			Intent gotoViewIndivProg = new Intent(EvaluateProgActivity.this, ViewIndivProgActivity.class);
+			startActivity(gotoViewIndivProg);
+			finish();
+		});
 	}
 }
