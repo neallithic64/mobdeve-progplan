0388000e0ae31a2a442178124e488c34611aad57
Andre Garcia <61376752+Episki2001@users.noreply.github.com>
2021-09-02 17:20:47 +0800

    Created filter programs modal(all views complete)
    TODO: Fix issue with view program button recycler view in ViewProgsActivity

--- app/src/main/java/com/mobdeve/s17/lim/matthew/mobdeve_progplan/FilterDialogFragment.java
+++ app/src/main/java/com/mobdeve/s17/lim/matthew/mobdeve_progplan/FilterDialogFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import com.mobdeve.s17.lim.matthew.mobdeve_progplan.databinding.FragmentFilterDialogBinding;
 
@@ -25,6 +26,16 @@ public class FilterDialogFragment extends DialogFragment {
 		// Inflate the layout for this fragment
 		binding = FragmentFilterDialogBinding.inflate(inflater, container, false);
 		View view = binding.getRoot();
+
+		ArrayAdapter<CharSequence> cityadapter = ArrayAdapter.createFromResource(getContext(),
+				R.array.city_array, android.R.layout.simple_spinner_item);
+		cityadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		binding.spinnerCity.setAdapter(cityadapter);
+
+		binding.btnFilterprograms.setOnClickListener(v -> {
+			dismiss();
+		});
+
 		return view;
 	}
 }
