476fdd87a12f1842b0c903e04bf3bfe70c1bc16d
Andre Garcia <61376752+Episki2001@users.noreply.github.com>
2021-09-03 23:15:22 +0800

    map api showing

--- app/src/main/java/com/mobdeve/s17/lim/matthew/mobdeve_progplan/MapDialogFragment.java
+++ app/src/main/java/com/mobdeve/s17/lim/matthew/mobdeve_progplan/MapDialogFragment.java
@@ -30,10 +30,10 @@ public class MapDialogFragment extends DialogFragment
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-//		View rootView = inflater.inflate(R.layout.fragment_map_dialog, container, false);
-		View rootView = inflater.inflate(R.layout.fragment_map_temp, container, false);
+		View rootView = inflater.inflate(R.layout.fragment_map_dialog, container, false);
+//		View rootView = inflater.inflate(R.layout.fragment_map_temp, container, false);
 
-//		SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
+		SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
 //		mapFragment.getMapAsync(this);
 		btn_selectLocation = rootView.findViewById(R.id.btn_selectLocation);
 		btn_selectLocation.setOnClickListener(v->{
@@ -42,18 +42,19 @@ public class MapDialogFragment extends DialogFragment
 		return rootView;
 	}
 
+//	TODO : Make it so the marker that leads to current locations work
 	@Override
 	public void onMapReady(GoogleMap googleMap) {
-//		mMap = googleMap;
-//		mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-//
-//		LatLng latLng = new LatLng(37.7688472,-122.4130859);
-//		mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,11));
-//
-//		MarkerOptions markerOptions = new MarkerOptions();
-//		markerOptions.position(latLng);
-//		markerOptions.title("Current Position");
-//		markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
-//		mMap.addMarker(markerOptions);
+		mMap = googleMap;
+		mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+		LatLng latLng = new LatLng(37.7688472,-122.4130859);
+		mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,11));
+
+		MarkerOptions markerOptions = new MarkerOptions();
+		markerOptions.position(latLng);
+		markerOptions.title("Current Position");
+		markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+		mMap.addMarker(markerOptions);
 	}
 }
