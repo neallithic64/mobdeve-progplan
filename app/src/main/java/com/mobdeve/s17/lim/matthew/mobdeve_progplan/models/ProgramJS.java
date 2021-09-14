de19f4e100c0069570f0037b5b8cf5ce2c45cf59
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2021-09-14 20:33:44 +0800

    made container class for Java-JS translation (#3)
    included documentation and explanation in the java file

---
+++ app/src/main/java/com/mobdeve/s17/lim/matthew/mobdeve_progplan/models/ProgramJS.java
@@ -0,0 +1,29 @@
+package com.mobdeve.s17.lim.matthew.mobdeve_progplan.models;
+
+import java.util.ArrayList;
+
+/* This is a class that's used to model a complate JOIN-ed program object. The
+	purpose of this class is NOT as a storage in the database, but rather used
+	to standardise the requests to and responses from the server. Thus, the
+	server response also needs to be formatted in an identical manner. I also
+	believe there won't be any setters here -- only getters.
+*/
+
+public class ProgramJS {
+	private Program program;
+	private ArrayList<Outcome> outcomes;
+	private ArrayList<Resource> resources;
+	private ArrayList<ProgChecklist> checklistItems;
+
+	public ProgramJS(Program program, ArrayList<Outcome> outcomes, ArrayList<Resource> resources, ArrayList<ProgChecklist> checklistItems) {
+		this.program = program;
+		this.outcomes = outcomes;
+		this.resources = resources;
+		this.checklistItems = checklistItems;
+	}
+
+	public Program getProgram() { return program; }
+	public ArrayList<Outcome> getOutcomes() { return outcomes; }
+	public ArrayList<Resource> getResources() { return resources; }
+	public ArrayList<ProgChecklist> getChecklist() { return checklistItems; }
+}
